60b7eaa78f326797002cb13feec4a1365f176aa0
km310765 <km310765@tamu.edu>
2013-09-09 17:25:54 -0500

    Update Database.h

--- Database.h
+++ Database.h
@@ -13,8 +13,9 @@ class Database {
 	Database(){Selection.name = "Selection"; Projection.name = "Projection";};
 	//Database(){};
     Table table;
-     void Select(string attr_name, string condition, string cell_condition, string rel_name);
-	 void Update(string rel_name, string attr_name, string literal, string condition_attr, string condition, string condition_literal);
+    void Select(string attr_name, string condition, string cell_condition, string rel_name);
+	void Update(string rel_name, string attr_name, string literal, string condition_attr, string condition, string condition_literal);
+	void Delete(string rel_name, string condition_attr, string condition, string condition_literal);
 	void Project(vector<string> attr_name, string rel_name);
     void Create(string rel_name);
 	void Delete(string rel_name);
@@ -25,7 +26,7 @@ class Database {
     void Rename(string rel_name,string new_name, string old_name);
     Relation& operator[](const string& s);
     void Delete_attr(const string& rel_name,const string& attribute);
-    void Union(const string& rel_name1,const string& rel_name2);
+    void Union(const string& rel_name1,const string& rel_name2, const string& rel_name3);
 	void Difference(const string& rel_name1, const string& rel_name2, const string& rel_name3);
 	void Product(const string& rel_name1, const string& rel_name2, const string& rel_name3);
 };
