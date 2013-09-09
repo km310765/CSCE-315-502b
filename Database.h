508af6b8917b5ad186f6f754b4da41e4b71c189b
km310765 <km310765@tamu.edu>
2013-09-09 00:00:27 -0500

    Create Database.h

---
+++ Database.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include "Relation.h"
+
+using namespace std;
+
+class Database {
+  public:
+    typedef map<string,Relation> Table;
+    vector<Relation> relation;
+  Database(){};
+    Table table;
+    void Selection(Relation name, string attr_name);
+    void Create(string rel_name);
+	void Delete(string rel_name);
+    void AddColumn(const string& rel_name, const Header& h);
+    void Insert(string rel_name, const vector<Cell>& row);
+    void Write(const string& rel_name);
+    void Show(string rel_name);
+    void Rename(string rel_name,string new_name, string old_name);
+    Relation& operator[](const string& s);
+    void Delete_attr(const string& rel_name,const string& attribute);
+    void Union(const string& rel_name1,const string& rel_name2);
+	void Difference(const string& rel_name1, const string& rel_name2, const string& rel_name3);
+	void Product(const string& rel_name1, const string& rel_name2, const string& rel_name3);
+};
