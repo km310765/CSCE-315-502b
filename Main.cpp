83fed8cf2582d051484a6b91d319c53281d739a1
km310765 <km310765@tamu.edu>
2013-09-09 22:12:40 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,57 +2,12 @@
 #include "Relation.h"
 #include "Database.h"
 #include "Attribute.h"
+#include "Token.h"
 
 using namespace std;
 
 int main(){
-  Database d;
-  /*d.Create("Cars");
-  d.AddColumn("Cars",Header("Make",STRING));
-  d.AddColumn("Cars",Header("Year",INT));
-  d.AddColumn("Cars",Header("Model",STRING));
-  vector<Cell> cell;
-  cell.push_back("Audi");
-  cell.push_back("2013");
-  cell.push_back("R8");
-  d.Insert("Cars", cell);
-  cell.clear();
-  cell.push_back("Audi2");
-  cell.push_back("2013222222");
-  cell.push_back("R8222222");
-  d.Insert("Cars", cell);
-
-  d.Create("LOL");
-  d.AddColumn("LOL",Header("Hello",STRING));
-  d.AddColumn("LOL",Header("Bello",STRING));
-  d.AddColumn("LOL",Header("haha",STRING));
-  
-  vector<Cell> cell1;
-  vector<Cell> cell2;
-  cell1.push_back("lol");
-  cell1.push_back("lol2");
-  cell1.push_back("lol3");
-  cell2.push_back("Audi");
-  cell2.push_back("2013");
-  cell2.push_back("R0");
-  d.Insert("LOL",cell2);
-  d.Insert("LOL",cell1);
-
-  d.Show("Cars");
-  d.Rename("Cars","sfsd","Model");
-  //d.Delete_attr("Cars","Audi");
-  d.Delete_attr("Cars","Joinks");
-  d.Delete_attr("haha","Audi");
-  d.Show("Cars");
-  d.Show("LOL");
-  //d.Union("Cars","LOL");
-  //d.AddColumn("CarsLOLUnion",Header("Union",STRING));
-  //d.Show("CarsLOLUnion");
-  d.Show("kjlk");
-  d.Show("Cars");
-  d.Write("Cars");*/
-  
-  d.Create("Table1");
+  /*d.Create("Table1");
   d.AddColumn("Table1", Header("Str1", STRING));
   d.AddColumn("Table1", Header("Str2", STRING));
   d.AddColumn("Table1", Header("Str3", STRING));
@@ -114,6 +69,20 @@ int main(){
   
   d.Show("Table3");
   d.Update("Table3", "Str2", "6", "Str3", "==", "2.9");
-  d.Show("Table3");
+  d.Show("Table3");*/
+  Token_stream ts;
+  ts.ss.clear();
+  ts.ss.str("");
+  cout << "Input line: ";
+  string line;
+  getline(cin, line);
+  ts.ss << line;
+  while(true)
+  {
+	Token t = ts.get();
+	cout << t.kind << ", " << t.str << ", " << t.value << endl;
+	if(t.kind == ';')
+		break;
+  }
   return 0;
 }
