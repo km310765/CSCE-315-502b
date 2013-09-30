f6db09632d5579e52583536e8b3c65d85cec5db5
km310765 <km310765@users.noreply.github.com>
2013-09-30 18:14:14 -0500

    Update Attribute.h

--- Attribute.h
+++ Attribute.h
@@ -1,21 +1,45 @@
-#ifndef PARSER_H
-#define PARSER_H
-
+#ifndef ATTRIBUTE_H
+#define ATTRIBUTE_H
 #include <iostream>
-#include <fstream>
 #include <vector>
-#include <stdio.h>
-#include <stdlib.h>
-#include "Database.h"
 
-class Parser
+using namespace std;
+
+enum Type { INT = 1, STRING };
+
+struct Cell
+{
+	string value;
+	Type type;
+};
+
+class Attribute
 {
 private:
-	Database* d;
+	string name;
+	Type type;
+	vector<Cell> cells;
 public:
-	Parser() { d = new Database(); }
-	Parser(Database* datab) : d(datab) {}
-	void parse(string line, bool print);
+	Attribute() {}
+	Attribute(Type t, string n) : type(t), name(n) {}
+	
+	int size() { return cells.size(); }
+	string get_name() { return name; }
+	Type get_type() { return type; }
+	Cell get(int i) { return cells[i]; }
+	Cell operator[](size_t i) { return get(i); }
+	vector<Cell> get_cells() { return cells; }
+	void clear() { cells.clear(); }
+	void push_back(Cell c) { cells.push_back(c); }
+	void push_back(string v, Type t)
+	{
+		Cell c;
+		c.value = v;
+		c.type = t;
+		cells.push_back(c);
+	}
+	void insert(int i, Cell c) { cells.insert(cells.begin() + i, c); }
+	void set_name(string n) { name = n; }
 };
 
 #endif
